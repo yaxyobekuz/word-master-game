f24b8f5c065a5cf80d772625d475bc64050ef029
yaxyobekuz <pubgn9642@gmail.com>
2026-03-15 13:08:26 +0500

    feat: create HomePage component with BottomNavbar integration

---
+++ src/features/home/pages/HomePage.jsx
@@ -0,0 +1,12 @@
+import BottomNavbar from "@/shared/components/ui/BottomNavbar";
+
+const HomePage = () => {
+  return (
+    <div className="container">
+      Hello
+      <BottomNavbar />
+    </div>
+  );
+};
+
+export default HomePage;
