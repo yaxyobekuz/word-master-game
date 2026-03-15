42ce2c3bef929b7b0b86a24297651f9b0c376fb9
yaxyobekuz <pubgn9642@gmail.com>
2026-03-15 13:06:12 +0500

    feat: initialize project structure with configuration files and HTML template

---
+++ vite.config.js
@@ -0,0 +1,16 @@
+// Path module
+import path from "path";
+
+// Vite
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});
